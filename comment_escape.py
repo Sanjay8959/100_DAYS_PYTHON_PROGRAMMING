640920294296f4424e5612db8e1455943fcbcd7f
Sanjay Singh <148002958+Sanjay8959@users.noreply.github.com>
2026-04-20 04:45:00 +0530

    Add files via upload

---
+++ comment_escape.py
@@ -0,0 +1,6 @@
+print("hello im sanjay singh ", 67,89)
+# for your information the above line will print the string and the two numbers as they are without any error because in python we can print multiple items by separating them with commas. The output will be:
+#escape character 
+print("hello im sanjay singh \"the coder\"")
+# for new line we are used thne \n in python langusge 
+print("hello im coder \nand im a computer science engineering student ")
