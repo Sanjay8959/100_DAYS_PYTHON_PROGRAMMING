56650a45daccc11e98cf0e6301bd4c6a9a609dd0
Sanjay Singh <148002958+Sanjay8959@users.noreply.github.com>
2026-04-23 01:42:35 +0530

    Add files via upload

---
+++ complex.py
@@ -0,0 +1,25 @@
+#data types in python mainly contains by default the following data type
+#1    Numeric data type
+#2           1. int 
+#            2. float
+#3           3. complex
+#4    text data type 
+#5           1. string data type jisko ki double quote me likha jata hai ""
+#6    boolean data type
+#7           1. true    
+#            2. false
+#8    sequence data type
+#9           1. list    
+#            2. tuple
+
+# now im here talk about the complex
+# data type which will represent the real and imaginary data type so it 
+# will perform in python using the complex function and you will provuide its value also 
+a = complex(2,3)
+print(a)
+# yoou can assign your value real and imaginary wala 
+#so
+c=2
+d=5
+e=complex(c,d)
+print(e)
