f4d70d3c98b077772a1fea00a2907500980f23f8
Sanjay Singh <148002958+Sanjay8959@users.noreply.github.com>
2026-04-23 01:42:00 +0530

    Add files via upload

---
+++ datatype.py
@@ -0,0 +1,12 @@
+#if you want your variable contain which type of data so you can yse 
+# TYPE  fumction and known about easily 
+# the synttax of type function is 
+#print(type(variable ))
+print(type(5))
+print(type(5.6))
+print(type("sanjay singh"))
+
+#and one more thing here also which is more important for that
+a = 5
+b = "sanjay singh"
+print(a+b)
